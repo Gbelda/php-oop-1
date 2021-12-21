37dee022418bd82133c0f336c76686ec0d012a4c
gbelda <gbelda56@gmail.com>
2021-12-21 15:10:18 +0100

    create class construct

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<!-- // definita una classe ‘Movie’ -->
 
 <?php
 
+//  definita una classe ‘Movie’
 class Movie{
+
+    //  all'interno della classe sono dichiarate delle variabili d'istanza
+    public $title;
+    public $year;
+    public $rating;
+    public $genre;
+
+    // all'interno della classe è definito un costruttore
+
+    public function __construct(String $title, int $year, float $rating, String $genre)
+    {
+        $this.$title = $title;
+        $this.$year = $year;
+        $this.$rating = $rating;
+        $this.$genre = $genre;
+    }
 
     
 }
